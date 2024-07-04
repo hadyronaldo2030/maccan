f190a683d81555f394dcdbc4fde5a760df3ce8e6
Reem Mohamed <166386701+reem38519753@users.noreply.github.com>
2024-07-04 05:29:42 +0300

    maccan

---
+++ translate.js
@@ -0,0 +1,172 @@
+i18next.init({
+    lng: localStorage.getItem('language') || 'ar',
+    resources: {
+        en: {
+            translation: {
+                // logo
+                "M": " M ‌ ",
+                "CCaNE": " C C @ N E",
+                "MCCaNE": "MCC@NE",
+
+                "Home": "Home",
+                "About": "About",
+                "Payment": "Payment",
+                "contact": "Contact",
+                "Privacy": "Privacy Policy",
+                "Admin": "Admin",
+                "Dashbord": "Dashbord",
+                "Profile": "Profile",
+                "Pricing": "Pricing",
+                "General": "General",
+                "Settings": "Settings",
+                "Logout": "Logout",
+                "lang": " en ",
+                "number": " +90 541 623 10 19 ",
+
+                "Contact-Us": " Contact Us ",
+                "Email": " Email: ",
+                "tel": " tel:",
+                "Address": "Address",
+                "Address-p": " Assiut / in front of Al-Hilaly Mosque ",
+                "Menu": " Menu ",
+                "AboutUs": " About Us ",
+                "Services": " Services ",
+                "payment-plans": " payment plans ",
+                "Quick-Links": " Quick Links ",
+                "Privacy-Policy": " Privacy Policy ",
+                "Social-Media": " Social Media ",
+                "Copy-rights": " © 2024 Masar. All rights reserved by ",
+
+                // Header
+                "Hello": "HELLO ! I AM YUSEF DALATI",
+                "And I Am": "And I Am",
+                "THE OWNER OF MACAN COMPANIES": "THE OWNER OF MACAN COMPANIES",
+                "p-header": "Specialized in managing, studying and improving businesses and projects Using advanced marketing methods that keep pace with the times Put your hands on our accumulated and integrated experience and provide solutions Innovative and creative to avoid risks and advance projects To achieve maximum benefit. Passion and dedication to work is a secret The success of our business",
+                "download cv": "download cv",
+
+                // About
+                "DIGITAL MARKETING AGENCY": "DIGITAL MARKETING AGENCY",
+                "p-about": "Distinguished in the world of business in terms of its development and innovation in services Its progress. A leading company specialized in marketing services to its customers around the world. With 3 years experience. We strive to achieve our goals by developing... Effective strategies. We believe that a partnership based on trust and cooperation is... The foundation of sustainable success, and we work to build relationships based on understanding And effective communication.",
+
+                // Services
+                "Services-1": "Management All Social Media",
+                "p-1": "We offer innovative strategies And a distinguished management that helps you build a strong presence, increase the interaction of your followers and enhance your digital presence.",
+                "Services-2": "Launching advertising campaigns on global platforms",
+                "p-2": "(Facebook, Instagram, Snapchat, Platform X (with the creation of a professional strategy and plan",
+                "Services-3": "Create a complete marketing plan",
+                "p-3": "Supports the goals of the organization, and the development of a plan Advertising campaigns and a monthly publishing plan with high expertise and professionalism Achieve the required benefits with your word",
+                "Services-4": "Search Engine Optimization (SEO)",
+                "p-4": "Search the most popular keywords and write articles in a way ProfessionalSupports SEO rules according to the planMethodology",
+                "Services-5": "Logo design and visual identity",
+                "p-5": "McCann's expert team offers all kinds of designs and brochures And other materials and tools necessary to launch or renew your project Maintain the platform.",
+                "Services-6": "Content Creation Creativity",
+                "p-6": "Authoring content professionally and flexibly that suits all situations (advertising, Technical, analytical and others) and is filled with the different target groups to deliver The idea is for the targeted people easily and achieve the desired goal",
+
+            }
+        },
+        ar: {
+            translation: {
+            // logo
+                "M": "مـ",
+                "CCaNE": "كان",
+                "MCCaNE": "مكان",
+
+                "Home": "الرئيسية",
+                "About": "نبذه عنا",
+                "Payment": "طريقة الدفع",
+                "contact": "اتصل بنا",
+                "Privacy": "سياسة الخصوصية",
+                "Admin": "المسؤل",
+                "Dashbord": "وحدة التحكم",
+                "Profile": "الصفحة الشخصية",
+                "Pricing": "التسعيرة",
+                "General": "عام",
+                "Settings": "الاعدادت",
+                "Logout": "تسجيل خروج",
+                "lang": " ar ",
+                "number": " +90 541 623 10 19 ",
+                "number": "91 01 326 145 09+",
+                
+                
+                "Contact-Us": " اتصل بنا",
+                "Email": "البريد الاكترونى: ",
+                "tel": "الهاتف: ",
+                "Address-p": " اسيوط / امام مسجد الهلالى ",
+                "Address": " العنوان ",
+                "Menu": " القائمة ",
+                "AboutUs": " معلومات عنا ",
+                "Services": " خدمات ",
+                "payment-plans": " خطط الدفع ",
+                "Quick-Links": " روابط سريعة ",
+                "Privacy-Policy": " سياسة الخصوصية ",
+                "Social-Media": " التوصل الاجتماعى ",
+                "Copy-rights": "© جميع الحقوق محفوظه لدى ",
+
+                // Header
+                "Hello": "مرحبًا ! أنا يوسف دالاتي",
+                "And I Am": "وانا",
+                "THE OWNER OF MACAN COMPANIES": "مالك شركات مكان",
+                "p-header": "متخصصون في إدارة ودراسة وتحسين الأعمال والمشاريع باستخدام أساليب تسويقية متطورة تواكب العصر ضع يديك على خبرتنا المتراكمة والمتكاملة ونقدم حلولاً مبتكرة ومبتكرة لتجنب المخاطر والنهوض بالمشاريع لتحقيق أقصى استفادة. الشغف والتفاني في العمل سر نجاح أعمالنا",
+                "download cv": "تحميل السيرة الذاتية",
+                
+                // About
+                "DIGITAL MARKETING AGENCY": "وكالة التسويق الرقمي",
+                "p-about": "متميزون في عالم الأعمال من حيث تطورها وابتكارها في الخدمات تقدمها. شركة رائدة متخصصة في تسويق الخدمات لعملائها في جميع أنحاء عالم. مع خبرة 3 سنوات. نحن نسعى جاهدين لتحقيق هدفنا من خلال تطوير... استراتيجيات فعالة. ونحن نعتقد أن الشراكة المبني على الثقة والتعاون.. أساس النجاح المستدام، ونحن نعمل لبناء علاقات مبنية على التفاهم والتواصل الفعال .",
+
+                // Services
+                "Services-1": "إدارة جميع وسائل التواصل الاجتماعي",
+                "p-1": "نقدم استراتيجيات مبتكرة وإدارة متميزة تساعدك على بناء حضور قوي وزيادة تفاعل متابعيك وتعزيز حضورك الرقمي.",
+                "Services-2": "إطلاق الحملات الإعلانية على المنصات العالمية",
+                "p-2": "(Facebook، Instagram، Snapchat، Platform X (مع إنشاء استراتيجية وخطة احترافية",
+                "Services-3": "إنشاء خطة تسويقية كاملة",
+                "p-3": "يدعم أهداف المنظمة، ووضع خطة حملات إعلانية وخطة نشر شهرية بخبرة واحترافية عالية تحقيق الفوائد المطلوبة بكلمتك",
+                "Services-4": "تحسين محركات البحث (SEO)",
+                "p-4": "ابحث عن الكلمات الرئيسية الأكثر شيوعًا واكتب المقالات بطريقة احترافية تدعم قواعد SEO وفقًا لمنهجية الخطة",
+                "Services-5": "تصميم الشعارات والهوية البصرية",
+                "p-5": "يقدم فريق الخبراء في مكان جميع أنواع التصاميم والكتيبات وغيرها من المواد والأدوات اللازمة لإطلاق مشروعك أو تجديده، والحفاظ على المنصة.",
+                "Services-6": "الإبداع في صناعة المحتوى",
+                "p-6": "تأليف محتوى بشكل احترافي ومرن يناسب كافة المواقف (إعلاني، فني، تحليلي وغيرها) ومليء بالفئات المستهدفة المختلفة لإيصال الفكرة للأشخاص المستهدفين بسهولة وتحقيق الهدف المنشود",
+            }
+        }
+    }
+}, function (err, t) {
+    updateContent();
+});
+
+function changeLanguage(lang) {
+    i18next.changeLanguage(lang, function (err, t) {
+        localStorage.setItem('language', lang);
+        updateContent();
+    });
+}
+
+function updateContent() {
+    // تحديث الترجمة على ال navbar
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        element.innerHTML = i18next.t(element.getAttribute('data-i18n'));
+    });
+
+    // تحديث اتجاه العناصر HTML الأخرى
+    var direction = (i18next.language === 'ar') ? 'rtl' : 'ltr';
+    document.getElementById('direction').dir = direction;
+
+    // إظهار أو إخفاء العناصر بناءً على اللغة المحددة
+    var isArabic = (i18next.language === 'ar');
+    document.getElementById('englishText').style.display = isArabic ? 'none' : 'block';
+    document.getElementById('arabicText').style.display = isArabic ? 'block' : 'none';
+
+    // اعطاء margin الى عناصر ال features
+    var elements = document.getElementsByClassName('margin-3');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.margin = isArabic ? '8px 0 0 15px' : '8px 15px 0 0';
+    }
+                                                        
+}
+
+$(document).ready(function () {
+    // استدعاء الدالة عند تحميل الصفحة
+    updateContent();
+});
+
+
+document.addEventListener('DOMContentLoaded', updateContent);
